0bcd261da5813ef7ac65f20f31c9d7e0b0b8ee27
Gabriel R Morales <grmorale@gmail.com>
2017-05-18 18:37:10 -0700

    Refactor for wrapping

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,24 +1,23 @@
-word=gets.chomp
-num=gets.chomp.to_i
+#word=gets.chomp
+#num=gets.chomp.to_i
 def caesar_cipher(text, shift_factor)
 text=text.split("")
 empty=""
 #puts text
-text.each do |x|
+  text.each do |x|
   
-  if (x!=" ")
-  	  if (x.match(/^[[:alpha:]]$/))
-  	    if (x==x.upcase)
-  	      x=x.downcase	
-      	  empty+=(x.ord+shift_factor%26).chr.upcase
-      	else
-      	  empty+=(x.ord+shift_factor%26).chr
-      	end
-      end    
-  elsif (x==" ")
-      empty+=" "
-  end  
-end
+    if (x.match(/^[[:alpha:]]$/))
+    	x=x.ord+shift_factor%26
+      if (x>90&&x<97)||(x>123)
+        empty+=(x-26).chr
+      else
+        empty+=x.chr
+      end
+    else
+      empty+=x
+
+    end    
+  end
   puts empty
 end
 
